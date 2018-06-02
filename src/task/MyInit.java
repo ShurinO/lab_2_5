ce87b1aab10c1898e0a30f7208f81201921202f7
ShurinO <arestarx@ukr.net>
2018-06-02 14:52:35 +0300

    lab_2_5_2+MyInit.java

---
+++ src/task/MyInit.java
@@ -0,0 +1,17 @@
+package task;
+
+public class MyInit {
+    private int[] arr;
+    {
+        arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= (int) (100 * Math.random());
+        }
+    }
+    public void printArray(){
+        for (int i = 0; i < arr.length-1; i++) {
+            System.out.print(arr[i]+",");
+        }
+        System.out.print(arr[arr.length-1]);
+    }
+}
