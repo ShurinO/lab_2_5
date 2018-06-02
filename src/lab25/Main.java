42ee9eea0e44a4b71c3e3c2ef077ab9e0058b3ab
ShurinO <arestarx@ukr.net>
2018-06-02 14:49:48 +0300

    lab_2_5_2

--- src/lab25/Main.java
+++ src/lab25/Main.java
@@ -1,9 +1,17 @@
 package lab25;
 
+import task.MyInit;
 import task.MyInitTest;
 
 public class Main {
     public static void main(String[] args) {
         MyInitTest myInitTest = new MyInitTest();
+
+        //lab_2_5_2
+        MyInit myInit1 = new MyInit();
+        myInit1.printArray();
+        System.out.println();
+        MyInit myInit2 = new MyInit();
+        myInit2.printArray();
     }
 }
